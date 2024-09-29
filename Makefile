621a952d4229b7334181fefa8424f6c51659bb94
JooSeongMoon <tjdansld87@naver.com>
2024-09-29 04:31:47 +0000

    Replit 실행 테스트

--- Makefile
+++ Makefile
@@ -4,10 +4,6 @@ INCLUDE=-I./UE4-Math
 CXX = g++
 override CXXFLAGS += -g -Wall -Werror -Wno-unused-label -Wno-unused-function -Wno-unused-parameter -Wno-unused-variable $(INCLUDE)
 
-INC = $(./UE4-Math/)
-INC_PARAMS=$(foreach d, $(INC), -I$d)
-SRC_DIR = ./UE4-Math
-
 SRCS = $(shell find . -name '.ccls-cache' -type d -prune -o -type f -name '*.cpp' -print | sed -e 's/ /\\ /g')
 HEADERS = $(shell find . -name '.ccls-cache' -type d -prune -o -type f -name '*.h' -print)
 
--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
-// UE4-Math.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// UE4-Math.cpp : This file contains the 'main' function. Program execution
+// begins and ends there.
 //
 
-#include <iostream>
 #include "Math/Quat.h"
+#include <iostream>
 
-int main()
-{
-    auto q1 = UE4Math::FQuat(0.3, 0.3, 0.3, 0.3);
-    auto q2 = UE4Math::FQuat(0.4, 0.4, 0.4, 0.4);
-    std::cout << "Hello World!\n";
+int main() {
+  auto q1 = UE4Math::FQuat(0.3, 0.3, 0.3, 0.3);
+  auto q2 = UE4Math::FQuat(0.4, 0.4, 0.4, 0.4);
+  std::cout << "Hello World!\n" << q1.ToString() << q2.ToString();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//   5. Go to Project > Add New Item to create new code files, or Project > Add
+//   Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project
+//   and select the .sln file
